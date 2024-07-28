28293739312ff612d86c1aeefe5710cd36bf2eb9
Jaminggu <130431829+Jaminggu@users.noreply.github.com>
2024-07-28 21:03:59 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,13 @@
+function solution(array, commands) {
+    answer = [];
+    
+    for(let i = 0; i < commands.length; i ++) {
+        let temp = array.slice(commands[i][0] - 1, commands[i][1]);
+        temp = temp.sort((a, b) => a - b);
+        
+        answer.push(temp[commands[i][2] - 1]);
+    }
+        
+    
+    return answer;
+}
