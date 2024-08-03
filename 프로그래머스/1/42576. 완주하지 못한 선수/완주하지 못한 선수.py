e72ad54485b4599d087b3cde968c3f768b00c3eb
Jaminggu <130431829+Jaminggu@users.noreply.github.com>
2024-08-04 00:49:24 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 70.39 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,7 +2,6 @@ def solution(participant, completion):
     participant.sort()
     completion.sort()
     
-    for p, c in zip(participant, completion):
-        if p != c:
-            return p
-    return participant[-1]
+    for participant_person in participant:
+        if participant_person in completion: completion.remove(participant_person)
+        else: return participant_person
